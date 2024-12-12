5cc6c77b44ed6f8328397a5c6117b81f0ec92cc4
James Sherry <james.m.sherry@googlemail.com>
2024-12-12 18:35:44 +0000

    github actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-ignore its a demo app straight from the docs
   plugins: [react()],
   test: {
     environment: "jsdom",
